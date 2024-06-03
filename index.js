b54fa1151a44b4ea4726904c95e7bfb7c455e3ce
Jami <jamsheduddin03@gmail.com>
2024-06-03 19:40:26 +0600

    database connected

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDb = require("./config/connnectDb");
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-console.log(process.env.PORT);
+
+connectDb();
 
 app.get("/", async (req, res) => {
   res.send("welcome to auction server");
